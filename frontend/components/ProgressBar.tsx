c4553a7dfae6dd065c11a837dd4c8deabf2ac62a
dosbenjamin <dosbenjamin@icloud.com>
2022-01-23 15:52:39 +0100

    refactor: improve progress bar performance with quicksetter

--- frontend/components/ProgressBar.tsx
+++ frontend/components/ProgressBar.tsx
@@ -1,36 +1,35 @@
-import { useASScroll } from 'app/features/asscroll'
-import type ASScroll from 'app/features/asscroll/ASScroll'
-import gsap from 'gsap'
+import { ScrollEvent, useASScroll } from 'features/asscroll'
+import { gsap } from 'gsap'
 import { useEffect, useRef } from 'react'
+import lerp from 'utilities/lerp'
 
 const Scrollbar = () => {
-  const scrollbar = useRef<HTMLDivElement>(null)
-  const { scroll, scrollCallbacks } = useASScroll()
+  const speed = 0.1
 
   const scale = { x: 0 }
-  const setProgress = () => {
-    gsap.to(scrollbar.current, {
-      scaleX: scale.x
-    })
-  }
+  const progress = { x: 0 }
 
-  const onScroll = (
-    progress: number,
-    scroll: ASScroll
-  ) => scale.x = progress / scroll.maxScroll
+  const updateProgress: ScrollEvent = (
+    currentPos,
+    { maxScroll }
+  ) => progress.x = currentPos / maxScroll
+
+  const scrollbar = useRef<HTMLDivElement>(null)
+  const { scroll } = useASScroll({ onScroll: updateProgress })
 
   useEffect(() => {
-    gsap.ticker.add(setProgress)
-    scrollCallbacks.add(onScroll)
-
-    return () => {
-      gsap.ticker.remove(setProgress)
-      scrollCallbacks.delete(onScroll)
-    }
+    const scaleXSet = gsap.quickSetter(scrollbar.current, 'scaleX')
+    const scaleBar = () => scaleXSet(scale.x += lerp(progress.x, scale.x, speed))
+    gsap.ticker.add(scaleBar)
+    return () => gsap.ticker.remove(scaleBar)
   }, [scroll])
 
   return (
-    <div ref={scrollbar} className="fixed inset-x-0 bottom-0 z-10 h-2 origin-left bg-black"></div>
+    <div
+      ref={scrollbar}
+      className="fixed inset-x-0 bottom-0 z-10 h-2 origin-left bg-black"
+    >
+    </div>
   )
 }
 
