d631c2c3050e9743da3a3596e130a055d1117790
dosbenjamin <dosbenjamin@icloud.com>
2022-02-13 22:17:16 +0100

    feat: add responsive to layout component

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,14 +1,29 @@
+import clsx from 'clsx'
 import type { ReactNode } from 'react'
 
 type Props = {
   children: ReactNode
   className?: string
+  size?: keyof typeof sizes
 }
 
-const Layout = ({ children, className }: Props) => (
-  <section className={`flex-shrink-0 h-screen px-24 py-20 ${className}`}>
-    {children}
-  </section>
-)
+const sizes = {
+  'base': 'px-24 md:px-[4vw] portrait:py-[5vh]',
+  'lg': 'px-[16vw] md:px-[4vw] portrait:py-[15vh]'
+}
+
+const Layout = ({ children, className, size = 'base' }: Props) => {
+  const sizeClass = sizes[size]
+
+  return (
+    <section className={clsx(
+      'flex-shrink-0 h-screen py-[10vh]',
+      `${sizeClass} ${className}`,
+      'portrait:h-auto'
+    )}>
+      {children}
+    </section>
+  )
+}
 
 export default Layout
