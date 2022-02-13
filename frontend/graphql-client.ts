ce0a95f3bbe54741da3cc0134eed4b83e3dbdb41
dosbenjamin <dosbenjamin@icloud.com>
2022-02-13 22:16:25 +0100

    chore: add token to graphql client

--- frontend/graphql-client.ts
+++ frontend/graphql-client.ts
@@ -1,4 +1,9 @@
 import { GraphQLClient } from 'graphql-request'
 
-const graphQLClient = new GraphQLClient(process.env.STRAPI_GRAPHQL_URL as string)
+const graphQLClient = new GraphQLClient(process.env.STRAPI_GRAPHQL_URL as string, {
+  headers: {
+    authorization: `Bearer ${process.env.STRAPI_TOKEN}`,
+  }
+})
+
 export default graphQLClient
